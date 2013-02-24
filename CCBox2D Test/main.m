b8ec2010ee0a6ccaae5907d80a6c43e1333f3a65
John Pope <jp@bellgeorge.com>
2013-02-24 17:30:40 +1100

    Initial Commit

---
+++ CCBox2D Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CCBox2D Test
+//
+//  Created by John Pope on 24/02/13.
+//  Copyright (c) 2013 Bell George. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGAppDelegate class]));
+    }
+}
