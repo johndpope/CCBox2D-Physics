0b41acb3471f995c6151b026791fe12b62a896c2
John Pope <jp@bellgeorge.com>
2013-02-26 15:14:47 +1100

    apply force is ok.

--- CCBox2D Test/CCBox2DTests/CCApplyForce.mm
+++ CCBox2D Test/CCBox2DTests/CCApplyForce.mm
@@ -1,5 +1,7 @@
 #import "CCApplyForce.h"
 #import "CCBodySprite.h"
+#import "CCJointSprite.h"
+#import "CCSpringSprite.h"
 
 @implementation CCApplyForce{
     
@@ -12,57 +14,57 @@
     
     if  (self!=nil){
 
-        m_world->SetGravity(b2Vec2(0.0f, 0.0f));
+        m_world->SetGravity(b2Vec2(0.0f, -1.0f));
         
-		const float32 k_restitution = 0.4f;
+		const float32 k_restitution = 5.4f;
+
         
-		b2Body* ground;
+        // Define the ground box shape.
+        CCBodySprite *ground;
 		{
-			b2BodyDef bd;
-			bd.position.Set(0.0f, 20.0f);
-			ground = m_world->CreateBody(&bd);
-            
-			b2EdgeShape shape;
-            
-			b2FixtureDef sd;
-			sd.shape = &shape;
-			sd.density = 0.0f;
-			sd.restitution = k_restitution;
+            ground = [[CCBodySprite alloc]initWithWorld:m_world bodyType:b2_staticBody];
+            ground.position = ccp(0.0f, 10.0f);
+            //[self addChild:ground];
+            [ground createBody];
             
 			// Left vertical
-			shape.Set(b2Vec2(-20.0f, -20.0f), b2Vec2(-20.0f, 20.0f));
-			ground->CreateFixture(&sd);
+           CCShape *leftEdge = [CCShape edgeWithVec1:b2Vec2(-20.0f, -20.0f)  vec2:b2Vec2(-20.0f, 20.0f)];
+            leftEdge.density = 0.0f;
+            leftEdge.restitution =k_restitution;
+            [ground addShape:leftEdge named:@"leftEdge"];
             
 			// Right vertical
-			shape.Set(b2Vec2(20.0f, -20.0f), b2Vec2(20.0f, 20.0f));
-			ground->CreateFixture(&sd);
-            
+            CCShape *rightEdge = [CCShape edgeWithVec1:b2Vec2(20.0f, -20.0f)  vec2:b2Vec2(20.0f, 20.0f)];
+            rightEdge.density = 0.0f;
+            rightEdge.restitution =k_restitution;
+            [ground addShape:rightEdge named:@"rightEdge"];
+
 			// Top horizontal
-			shape.Set(b2Vec2(-20.0f, 20.0f), b2Vec2(20.0f, 20.0f));
-			ground->CreateFixture(&sd);
-            
-			// Bottom horizontal
-			shape.Set(b2Vec2(-20.0f, -20.0f), b2Vec2(20.0f, -20.0f));
-			ground->CreateFixture(&sd);
+             CCShape *topEdge = [CCShape edgeWithVec1:b2Vec2(-20.0f, 20.0f)  vec2:b2Vec2(20.0f, 20.0f)];
+             topEdge.density = 0.0f;
+             topEdge.restitution =k_restitution;
+             [ground addShape:topEdge named:@"topEdge"];
+             
+			// Bottom horizontal            
+            CCShape *bottomEdge = [CCShape edgeWithVec1:b2Vec2(-20.0f, -20.0f)  vec2:b2Vec2(20.0f, -20.0f)];
+            bottomEdge.density = 0.0f;
+            bottomEdge.restitution =k_restitution;
+            [ground addShape:bottomEdge named:@"bottomEdge"];
 		}
         
 		{
 			b2Transform xf1;
 			xf1.q.Set(0.3524f * b2_pi);
 			xf1.p = xf1.q.GetXAxis();
-            
-			b2Vec2 vertices[3];
+
+            b2Vec2 vertices[3];
 			vertices[0] = b2Mul(xf1, b2Vec2(-1.0f, 0.0f));
 			vertices[1] = b2Mul(xf1, b2Vec2(1.0f, 0.0f));
 			vertices[2] = b2Mul(xf1, b2Vec2(0.0f, 0.5f));
             
-			b2PolygonShape poly1;
-			poly1.Set(vertices, 3);
-            
-			b2FixtureDef sd1;
-			sd1.shape = &poly1;
-			sd1.density = 4.0f;
-            
+            CCShape *poly = [CCShape polygonWithVecVertices:vertices count:3];
+			poly.density = 4.0f; 
+
 			b2Transform xf2;
 			xf2.q.Set(-0.3524f * b2_pi);
 			xf2.p = -xf2.q.GetXAxis();
@@ -71,28 +73,23 @@
 			vertices[1] = b2Mul(xf2, b2Vec2(1.0f, 0.0f));
 			vertices[2] = b2Mul(xf2, b2Vec2(0.0f, 0.5f));
             
-			b2PolygonShape poly2;
-			poly2.Set(vertices, 3);
-            
-			b2FixtureDef sd2;
-			sd2.shape = &poly2;
-			sd2.density = 2.0f;
-            
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
+            CCShape *poly2 = [CCShape polygonWithVecVertices:vertices count:3];
+			poly2.density = 2.0f;
+
+            CCBodySprite *bd = [[CCBodySprite alloc]initWithWorld:m_world bodyType:b2_dynamicBody];
 			bd.angularDamping = 5.0f;
-			bd.linearDamping = 0.1f;
+			bd.damping = 0.1f;
+            bd.rotation = b2_pi;
+            bd.position = ccp(0.0f, 2.0);
+            bd.sleepy = NO;
             
-			bd.position.Set(0.0f, 2.0);
-			bd.angle = b2_pi;
-			bd.allowSleep = false;
-			m_body = m_world->CreateBody(&bd);
-			m_body->CreateFixture(&sd1);
-			m_body->CreateFixture(&sd2);
+            [bd addShape:poly named:@"poly"];
+            [bd addShape:poly2 named:@"poly2"];
+
 		}
         
 		{
-			b2PolygonShape shape;
+            b2PolygonShape shape;
 			shape.SetAsBox(0.5f, 0.5f);
             
 			b2FixtureDef fd;
@@ -102,43 +99,39 @@
             
 			for (int i = 0; i < 10; ++i)
 			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-                
-				bd.position.Set(0.0f, 5.0f + 1.54f * i);
-				b2Body* body = m_world->CreateBody(&bd);
-                
-				body->CreateFixture(&fd);
-                
+                CCBodySprite *bd = [[CCBodySprite alloc]initWithWorld:m_world bodyType:b2_dynamicBody];
+                bd.position = ccp(0.0f, 5.0f + 1.54f * i);
+   
+                CCShape *mShape = [CCShape boxWithFixtureDef:fd];
+                [bd addShape:mShape named:[NSString stringWithFormat:@"fd%d",i]];
+
 				float32 gravity = 10.0f;
-				float32 I = body->GetInertia();
-				float32 mass = body->GetMass();
+				float32 I = bd.body->GetInertia();
+				float32 mass = bd.body->GetMass();
                 
 				// For a circle: I = 0.5 * m * r * r ==> r = sqrt(2 * I / m)
 				float32 radius = b2Sqrt(2.0f * I / mass);
-                
-				b2FrictionJointDef jd;
+
+				//b2FrictionJointDef jd;
+                b2FrictionJointDef jd;
 				jd.localAnchorA.SetZero();
 				jd.localAnchorB.SetZero();
-				jd.bodyA = ground;
-				jd.bodyB = body;
-				jd.collideConnected = true;
+				jd.bodyA = ground.body;
+				jd.bodyB = bd.body;
+				jd.collideConnected = false;
 				jd.maxForce = mass * gravity;
 				jd.maxTorque = mass * radius * gravity;
+
+                //CCSpringSprite *jointSprite = [[CCSpringSprite alloc] initWithWorld:m_world distanceJointDef:jd body1:ground body2:bd];
+                //[jointSprite createJoint];
+                m_world->CreateJoint(&jd);
+
                 
-				m_world->CreateJoint(&jd);
 			}
 		}
 
         
-        /*CCBodySprite *bd = [[CCBodySprite alloc]init];
-         CCShape *polygonShape = [CCShape boxWithRect:CGRectMake(0, 0, 1.5f, 1.5f)];
-         [bd addShape:polygonShape named:@"polygonShape"];
-         //[polygonShape addFixtureToBody:bd];
-         bd.position = ccp(-40.0f, 5.0f);
-         bd.bullet = YES;
-         [bd setVelocity:ccp(150.0f, 0.0f)];
-        [self addChild:bd];*/
+
     }
     return self;
 }
